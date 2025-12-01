1da0d3b2601771e453294adc051bdbdf297d59ba
Vincenzo Di Vita <121858876+vincenzodivita@users.noreply.github.com>
2025-12-01 17:43:47 +0100

    Add SongsModule and SetlistsModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { FirestoreModule } from './firestore/firestore.module';
+import { SongsModule } from './songs/songs.module';
+import { SetlistsModule } from './setlists/setlists.module';
 
 @Module({
-  imports: [FirestoreModule, AuthModule],
+  imports: [FirestoreModule, AuthModule, SongsModule, SetlistsModule],
 })
 export class AppModule {}
